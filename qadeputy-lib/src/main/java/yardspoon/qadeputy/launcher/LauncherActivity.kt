93879f939a9de953d0d5526ed2238e3205c5a379
Benjamin P Lee <benjaminplee@gmail.com>
2018-04-14 15:01:58 -0500

    Cleanup

--- qadeputy-lib/src/main/java/yardspoon/qadeputy/launcher/LauncherActivity.kt
+++ qadeputy-lib/src/main/java/yardspoon/qadeputy/launcher/LauncherActivity.kt
@@ -2,17 +2,17 @@ package yardspoon.qadeputy.launcher
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_launcher.*
+import kotlinx.android.synthetic.main.qadeputy_activity_launcher.*
 import yardspoon.qadeputy.R
 import yardspoon.qadeputy.logcat.logLogs
 
 class LauncherActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_launcher)
+        setContentView(R.layout.qadeputy_activity_launcher)
 
-        button.setOnClickListener {
-            logLogs()
-        }
+        button.setOnClickListener { logLogs() }
     }
+
 }
