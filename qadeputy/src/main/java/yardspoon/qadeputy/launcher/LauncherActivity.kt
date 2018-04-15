fc61efb63183e354c5313c89c2cc7d32e600b4bf
Benjamin P Lee <benjaminplee@gmail.com>
2018-04-15 15:00:19 -0500

    Touchable targets

--- qadeputy/src/main/java/yardspoon/qadeputy/launcher/LauncherActivity.kt
+++ qadeputy/src/main/java/yardspoon/qadeputy/launcher/LauncherActivity.kt
@@ -2,9 +2,9 @@ package yardspoon.qadeputy.launcher
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.qadeputy_activity_launcher.*
 import yardspoon.qadeputy.R
-import yardspoon.qadeputy.logcat.logLogs
 
 class LauncherActivity : AppCompatActivity() {
 
@@ -12,7 +12,10 @@ class LauncherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.qadeputy_activity_launcher)
 
-        QADeputy_logsIcon.setOnClickListener { logLogs() }
+        QADeputy_infoIcon.setOnClickListener { Toast.makeText(this, "Launch App Info", Toast.LENGTH_SHORT).show() }
+        QADeputy_crashIcon.setOnClickListener { Toast.makeText(this, "Launch Test Crash", Toast.LENGTH_SHORT).show() }
+        QADeputy_logsIcon.setOnClickListener { Toast.makeText(this, "Launch Log Share", Toast.LENGTH_SHORT).show() }
+        QADeputy_captureIcon.setOnClickListener { Toast.makeText(this, "Launch Screenshot Capture", Toast.LENGTH_SHORT).show() }
     }
 
 }
