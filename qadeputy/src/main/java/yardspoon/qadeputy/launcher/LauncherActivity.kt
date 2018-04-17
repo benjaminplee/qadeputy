9233687fb3aeb69611efc94e0d70c0a4a1516370
Benjamin P Lee <benjaminplee@gmail.com>
2018-04-16 21:31:27 -0500

    Working on log output

--- qadeputy/src/main/java/yardspoon/qadeputy/launcher/LauncherActivity.kt
+++ qadeputy/src/main/java/yardspoon/qadeputy/launcher/LauncherActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.qadeputy_activity_launcher.*
 import yardspoon.qadeputy.R
+import yardspoon.qadeputy.logcat.dumpLogs
 
 class LauncherActivity : AppCompatActivity() {
 
@@ -14,7 +15,10 @@ class LauncherActivity : AppCompatActivity() {
 
         QADeputy_infoIcon.setOnClickListener { Toast.makeText(this, "Launch App Info", Toast.LENGTH_SHORT).show() }
         QADeputy_crashIcon.setOnClickListener { Toast.makeText(this, "Launch Test Crash", Toast.LENGTH_SHORT).show() }
-        QADeputy_logsIcon.setOnClickListener { Toast.makeText(this, "Launch Log Share", Toast.LENGTH_SHORT).show() }
+        QADeputy_logsIcon.setOnClickListener {
+            dumpLogs()
+            Toast.makeText(this, "Launch Log Share", Toast.LENGTH_SHORT).show()
+        }
         QADeputy_captureIcon.setOnClickListener { Toast.makeText(this, "Launch Screenshot Capture", Toast.LENGTH_SHORT).show() }
     }
 
--- qadeputy/src/main/java/yardspoon/qadeputy/logcat/LogcatUtils.kt
+++ qadeputy/src/main/java/yardspoon/qadeputy/logcat/LogcatUtils.kt
@@ -1,16 +1,46 @@
 package yardspoon.qadeputy.logcat
 
+import android.os.Environment
 import timber.log.Timber
+import java.io.File
 import java.io.IOException
 
 
-fun logLogs() {
-    try {
-        val command = arrayOf("logcat", "-t", "10")
-        Runtime.getRuntime().exec(command).inputStream.buffered().reader().forEachLine {
-            Timber.i(it)
+fun dumpLogs() {
+    if (isExternalStorageWritable()) {
+        getLogFile()?.let {
+            run(arrayOf("logcat", "-f", it.absolutePath))
         }
-    } catch (ioe: IOException) {
-        Timber.e(ioe, "getAppLogs failed")
     }
 }
+
+private fun run(command: Array<String>) {
+    try {
+        Runtime.getRuntime().exec(command).inputStream.buffered().reader().forEachLine {
+            Timber.d("Exec > $it")
+        }
+    } catch (ioe: IOException) {
+        Timber.e(ioe, "Command failed")
+    }
+}
+
+private const val logFileName = "logs"
+
+fun getLogFile(): File? {
+    return File(getPublicStorageDirectory(), logFileName)
+}
+
+private const val storageDirectory = "qadeputy"
+
+fun getPublicStorageDirectory(): File? {
+    val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), storageDirectory)
+
+    if (!file.exists() && !file.mkdirs()) {
+        Timber.e("Directory, ${file.path}, not created")
+    }
+
+    return file
+}
+
+fun isExternalStorageWritable() = Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
+fun isExternalStorageReadable() = Environment.getExternalStorageState() in setOf(Environment.MEDIA_MOUNTED, Environment.MEDIA_MOUNTED_READ_ONLY)
